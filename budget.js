8cb8650ae5ef354c10fd0f425f9ab26f510e155a
Juan Sanchez <juansanchez2015@gmail.com>
2020-03-22 16:20:15 -0400

    Commented code in js file

--- budget.js
+++ budget.js
@@ -8,15 +8,15 @@
 
 var list = document.getElementById("myUL");
 
-var myNodelist = document.getElementsByTagName("LI");
-var i;
-for (i = 0; i < myNodelist.length; i++) {
-  var span = document.createElement("SPAN");
-  var txt = document.createTextNode("\u00D7");
-  span.className = "close";
-  span.appendChild(txt);
-  myNodelist[i].appendChild(span);
-}
+// var myNodelist = document.getElementsByTagName("LI");
+// var i;
+// for (i = 0; i < myNodelist.length; i++) {
+//   var span = document.createElement("SPAN");
+//   var txt = document.createTextNode("\u00D7");
+//   span.className = "close";
+//   span.appendChild(txt);
+//   myNodelist[i].appendChild(span);
+// }
 
 function addElement(color) {
 
@@ -80,12 +80,12 @@ function addElement(color) {
   li.appendChild(span);
 
 
-  for (i = 0; i < close.length; i++) {
-    close[i].onclick = function() {
-      var div = this.parentElement;
-      div.style.display = "none";
-    }
-  }
+  // for (i = 0; i < close.length; i++) {
+  //   close[i].onclick = function() {
+  //     var div = this.parentElement;
+  //     div.style.display = "none";
+  //   }
+  // }
 }
 
 // function addElement(colo, exp, nu){
